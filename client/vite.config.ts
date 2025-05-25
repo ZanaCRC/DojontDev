bd42dd8cde206c0e8d23418f01b609fc0c20fd85
Jeremy Pizarro Chaves <zanacr2204@gmail.com>
2025-05-25 10:28:46 -0600

    Viteconf

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,11 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import wasm from 'vite-plugin-wasm';
 import mkcert from 'vite-plugin-mkcert'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), wasm(),mkcert()],
+  plugins: [react(), tailwindcss(), wasm(), mkcert()],
+  server: {
+    allowedHosts: ['dojontapp.ticocr.org'],
+  },
 })
