75c85f68ed835baa916e887d94ae73172c09c983
Alessandro1809 <dalessandro18dg@gmail.com>
2025-05-31 19:02:19 -0600

    corrigiendo  linter errors 3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import wasm from 'vite-plugin-wasm';
 import mkcert from 'vite-plugin-mkcert'
-import fs from 'fs'
+
 
 // https://vite.dev/config/
 export default defineConfig({
